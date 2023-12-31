17f88b8cfbccb849b62ec4f13487782378696588
tsthuy <huydo282003@gmail.com>
2024-01-01 06:09:05 +0700

    cai dat xu ly loi

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const contactSRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -13,5 +14,15 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/contacts", contactSRouter);
+
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
 
 module.exports = app;
